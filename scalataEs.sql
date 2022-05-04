8813c5620f19702357778d07e4ad0e688a2a6696
michesichiara <michesichiara@gmail.com>
2022-05-04 12:18:47 +0200

    es 2

--- scalataEs.sql
+++ scalataEs.sql
@@ -1,5 +1,9 @@
 use scalatori;
 
-select distinct scalatore.CF, nazione.continente 
+
+select scalatore.CF, nazione.continente
 from scalatore join scalata on scalatore.CF = scalata.scalatore
-    join nazione
+    join nazione 
+where scalatore.annoNascita < 1980
+group by scalatore
+order by scalatore.CF, nazione.continente
